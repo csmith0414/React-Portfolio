7acfb8dba5c75db8dd6c3c280684d372cf97a0ce
Chad Smith <cds021@yahoo.com>
2023-11-21 21:16:51 -0500

    updated contact form, added image on about me page, input footer document

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,50 +1,20 @@
 const Contact = () => {
     return (
         <div>
-            <form className="row g-3">
-                <div className="col-md-6">
-                    <label for="inputEmail4" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="inputEmail4"/>
+            <div className="row">
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Name" aria-label="Name" />
                 </div>
-                <div className="col-md-6">
-                    <label for="inputPassword4" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="inputPassword4"/>
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Phone" aria-label="Phone" />
                 </div>
-                <div class="col-12">
-                    <label for="inputAddress" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Email" aria-label="Email" />
                 </div>
-                <div class="col-12">
-                    <label for="inputAddress2" className="form-label">Address 2</label>
-                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+                <div className="col">
+                    <input type="text" className="form-control" placeholder="Message" aria-label="Message" />
                 </div>
-                <div class="col-md-6">
-                    <label for="inputCity" className="form-label">City</label>
-                    <input type="text" className="form-control" id="inputCity"/>
-                </div>
-                <div class="col-md-4">
-                    <label for="inputState" className="form-label">State</label>
-                    <select id="inputState" className="form-select">
-                        <option selected>Choose...</option>
-                        <option>...</option>
-                    </select>
-                </div>
-                <div class="col-md-2">
-                    <label for="inputZip" className="form-label">Zip</label>
-                    <input type="text" className="form-control" id="inputZip"/>
-                </div>
-                <div className="col-12">
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" id="gridCheck"/>
-                            <label className="form-check-label" for="gridCheck">
-                                Check me out
-                            </label>
-                    </div>
-                </div>
-                <div className="col-12">
-                    <button type="submit" className="btn btn-primary">Sign in</button>
-                </div>
-            </form>
+            </div>
         </div>
     );
 };
